a8fce227d44e89ea6f4acf7bc502f8d58e015514
COLLABERA <COLLABERA@COLLABPH1001339>
2023-03-17 23:35:06 +0800

    Add project files.

---
+++ EMSWebApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace EMSWebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime dob { get; set; }
+        public string email { get; set; }
+        public string Phone { get; set; }
+        public department department { get; set; }
+
+    }
+}
---
+++ EMSWebApp/Models/department.cs
@@ -0,0 +1,9 @@
+﻿namespace EMSWebApp.Models
+{
+    public class department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
